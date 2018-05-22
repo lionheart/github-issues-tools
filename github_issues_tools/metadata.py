93d343117f2c97c9de124714e038e753a55e3d4c
Dan Loewenherz <dan@lionheartsw.com>
2018-05-22 09:47:02 -0500

    bump version to

--- github_issues_tools/metadata.py
+++ github_issues_tools/metadata.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "0.1.0"
+__version__ = ""
 __author__ = "Dan Loewenherz"
 __copyright__ = "Copyright 2018, Lionheart Software"
 __maintainer__ = "Dan Loewenherz"
